d9a6988b4c4328ada3c06b26044512cbee8cb374
Graziely Jacob <jacobgrazy@gmail.com>
2022-07-20 18:02:16 -0300

    Correção do package

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Title } from 'react';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system"> </div>
+      <div data-testid="solar-system">
+        <Title />
+      </div>
     );
   }
 }
