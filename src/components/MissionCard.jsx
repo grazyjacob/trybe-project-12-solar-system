6b1af0ad17438c5a75eb754e20679fca5392bafb
Graziely Jacob <jacobgrazy@gmail.com>
2022-07-25 09:14:34 -0300

    Lint Corrigido

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import PropType from 'prop-types';
+import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
       <div data-testid="mission-card">
-        <p data-testid="mission-name">{name}</p>
-        <p data-testid="mission-year">{year}</p>
-        <p data-testid="mission-country">{country}</p>
-        <p data-testid="mission-destination">{destination}</p>
+        <p data-testid="mission-name">{ name }</p>
+        <p data-testid="mission-year">{ year }</p>
+        <p data-testid="mission-country">{ country }</p>
+        <p data-testid="mission-destination">{ destination }</p>
       </div>
     );
   }
@@ -17,9 +17,9 @@ class MissionCard extends React.Component {
 
 export default MissionCard;
 
-MissionCard.proptype = {
-  name: PropType.string.isRequired,
-  year: PropType.string.isRequired,
-  country: PropType.string.isRequired,
-  destination: PropType.string.isRequired,
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
 };
